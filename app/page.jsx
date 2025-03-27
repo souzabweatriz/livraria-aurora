500425a9a01a0ecd5e7a2a877083b42affbf9f76
Isabella Borin <158210136+isab5@users.noreply.github.com>
2025-03-27 10:33:13 -0300

    feat: create folderinicio

---
+++ app/page.jsx
@@ -0,0 +1,19 @@
+"use client";
+import styles from "../styles/page.module.css";
+
+
+import { useEffect } from "react";
+import { useRouter } from "next/navigation";
+
+export default function Home() {
+    const router = useRouter();
+
+    useEffect(() => {
+        router.push("/inicio");
+    }, [router]);
+	
+return (
+	<p className={styles.text}>Redirecting …</p>
+);
+}
+
