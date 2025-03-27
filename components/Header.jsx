14a2d32cd3fbf140b792a860d0a3354b17b89b4e
Bea Souza <158209681+souzabweatriz@users.noreply.github.com>
2025-03-27 11:00:11 -0300

    feat: add img

---
+++ components/Header.jsx
@@ -0,0 +1,14 @@
+import styles from "../styles/Header.module.css"
+
+export default function Header(){
+    <header className={styles.header}>
+        <div className={styles.links}>
+            <li className={styles.list}> <a href="/">Início</a></li>
+            <li className={styles.list}> <a href="/">Livros</a></li>
+            <li className={styles.list}> <a href="/">Categorias</a></li>
+            <li className={styles.list}> <a href="/">Categorias</a></li>
+            <li className={styles.list}> <a href="/">Autores</a></li>
+            <li className={styles.list}> <a href="/">Ofertas</a></li>
+        </div>
+    </header>
+}
