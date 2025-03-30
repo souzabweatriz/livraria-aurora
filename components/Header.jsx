d6fa4358f98e1bec80b001ab630b2a9ad4162e48
Bea Souza <158209681+souzabweatriz@users.noreply.github.com>
2025-03-30 18:33:43 -0300

    feat: add component header

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,27 +1,32 @@
-import styles from "../styles/Header.module.css"
+import styles from "../styles/Header.module.css";
+import Image from "next/image";
+import Aurora from "../public/Aurora.png"
 
 export default function Header() {
     <header>
         <div className={styles.header}>
             <div className={styles.logo}>
-                <h2 className={styles.title}>
-
-                </h2>
+                <Image src={Aurora} alt="Logo da livraria" />
+                <h2 className={styles.title}>Livraria Aurora</h2>
             </div>
             <ul className={styles.links}>
-                <li className={styles.list}> <a href="/">Início</a></li>
-                <li className={styles.list}> <a href="/">Livros</a></li>
-                <li className={styles.list}> <a href="/">Categorias</a></li>
-                <li className={styles.list}> <a href="/">Categorias</a></li>
-                <li className={styles.list}> <a href="/">Autores</a></li>
-                <li className={styles.list}> <a href="/">Ofertas</a></li>
-            </ul>
-            <input className={styles.input} type="text" placeholder="Buscar" />
-            <ul className={styles.icons}>
                 <li className={styles.list}>
-                    
+                    <a href="/">Início</a>
+                </li>
+                <li className={styles.list}>
+                    <a href="/">Livros</a>
+                </li>
+                <li className={styles.list}>
+                    <a href="/">Categorias</a>
+                </li>
+                <li className={styles.list}>
+                    <a href="/">Autores</a>
+                </li>
+                <li className={styles.list}>
+                    <a href="/">Ofertas</a>
                 </li>
             </ul>
+            <input className={styles.input} type="text" placeholder="Buscar" />
         </div>
-    </header>
+    </header>;
 }
