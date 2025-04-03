d960b96c5ff8ecb02fe3858d3faee22008c69856
Isabella <isabellaborin.tds1.senai@gmail.com>
2025-04-02 22:29:37 -0300

    feat: create all footer

--- components/SocialList.jsx
+++ components/SocialList.jsx
@@ -2,10 +2,9 @@ import styles from "../styles/SocialCard.module.css";
 
 export default function SocialCard({ url, link, name }) {
     return (
-        <li>
-            <img src={url} alt=""/>
-
-            <a target="_blank" href={link}>{name}</a>
+        <li className={styles.container}>
+            <img className={styles.image} src={url} alt=""/>
+            <a className={styles.socialName} target="_blank" href={link}>{name}</a>
         </li>
     );
 }
