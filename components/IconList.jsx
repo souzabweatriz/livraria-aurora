59a668829437365907c259b9a261e51db0955cd2
Bea Souza <158209681+souzabweatriz@users.noreply.github.com>
2025-04-03 09:37:19 -0300

    feat: finish header

--- components/IconList.jsx
+++ components/IconList.jsx
@@ -1,16 +1,9 @@
-import React from "react";
-import { icons } from "../data/iconList";
+import styles from "../styles/IconList.module.css"
 
-export default function IconList() {
+export default function IconList({image}) {
     return (
-        <ul className={styles.icons}>
-            {icons.map((icon, index) => (
-                <li key={index}>
-                    <a href={icon.link}>
-                        <img src={icon.image} alt="icon" />
-                    </a>
-                </li>
-            ))}
-        </ul>
+        <li className={styles.container}>
+            <img className={styles.image} src={image} alt=""/>
+        </li>
     )
 }
